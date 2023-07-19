4af9ca7e78a838f62351cbc25d55df1c2e07c7e7
Jeovane Lima da Silva <jeovanelima2006@gmail.com>
2023-07-19 17:57:50 -0300

    finalizando o curso com o ultimo exercicio

--- exercicios/aula16ex/ex027/script.js
+++ exercicios/aula16ex/ex027/script.js
@@ -21,37 +21,50 @@ function inLista(n, l){
 
 
 function adicionar() {
+   
 
     if (isNum(num.value) && !inLista(num.value, valores)) {
         valores.push(Number(num.value))
         var item = document.createElement('option')
             item.text = `Valor ${num.value} foi adicionado`
             sel.appendChild(item)
+            res.innerHTML= ''
     }else{
         window.alert('[ERRO], número inválido ou já foi adicionado')
     }
+
+    num.value = ''
+    num.focus()
     
 }
 
 function finalizar(){
-    var maxValue = valores.reduce(function(prev, current) { 
-        return prev > current ? prev : current
-    })
 
-    var minValue = valores.reduce(function(prev, current) { 
-        return prev < current ? prev : current
-    })
-    
-    var soma = 0
-    for(var c = 0; c < valores.length;c++){
-        soma += valores[c]
+    if(valores.length == 0){
+        alert('Adicione valores antes de finalizar')
+    }else{
+        var maior = valores[0]
+        var menor = valores[0]
+
+        for (let pos  in valores) {
+            if(valores[pos] > maior)
+                maior = valores[pos]
+            if(valores[pos] < menor)
+                menor = valores[pos]
+        }
+
+        var soma = 0
+        for(var c = 0; c < valores.length;c++){
+            soma += valores[c]
+        }
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${valores.length} números cadastrados.</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
+        res.innerHTML += `<p>Somando todos os valores, temos ${soma}.</p>`
+        res.innerHTML += `<p>A média dos valores digitados é ${soma/valores.length}.</p>`
     }
-
-    res.innerHTML = `<p>Ao todo, temos ${valores.length} números cadastrados.</p>`
-    res.innerHTML += `<p>O maior valor informado foi ${maxValue}.</p>`
-    res.innerHTML += `<p>O menor valor informado foi ${minValue}.</p>`
-    res.innerHTML += `<p>Somando todos os valores, temos ${soma}.</p>`
-    res.innerHTML += `<p>A média dos valores digitados é ${soma/valores.length}.</p>`
+   
 }
 
 
