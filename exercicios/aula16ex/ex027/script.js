4f9c4a432f818306d8810d8e7607a113e6a46822
Jeovane Lima da Silva <jeovanelima2006@gmail.com>
2023-07-19 16:38:13 -0300

    finalizando ultimo exercicio

--- exercicios/aula16ex/ex027/script.js
+++ exercicios/aula16ex/ex027/script.js
@@ -3,20 +3,55 @@ var sel = document.getElementById('sel')
 var res = document.getElementById('res')
 var valores = []
 
+function isNum(n) {
+    if(Number(n) >= 1 && Number(n) <= 100){
+        return true
+    }else{
+        return false
+    }
+}
+
+function inLista(n, l){
+    if(l.indexOf(Number(n)) != -1){
+        return true
+    }else{
+        return false
+    }
+}
+
+
 function adicionar() {
 
-    if (num.value.length == 0 || num.value < 1 || num.value > 100 ) {
-        window.alert('ERRO, número inválido ou já foi adicionado')
-    }else{
-        var n = Number(num.value)
+    if (isNum(num.value) && !inLista(num.value, valores)) {
+        valores.push(Number(num.value))
         var item = document.createElement('option')
-            item.text = `numero ${n} foi adicionado`
+            item.text = `Valor ${num.value} foi adicionado`
             sel.appendChild(item)
-           
+    }else{
+        window.alert('[ERRO], número inválido ou já foi adicionado')
+    }
+    
+}
+
+function finalizar(){
+    var maxValue = valores.reduce(function(prev, current) { 
+        return prev > current ? prev : current
+    })
+
+    var minValue = valores.reduce(function(prev, current) { 
+        return prev < current ? prev : current
+    })
+    
+    var soma = 0
+    for(var c = 0; c < valores.length;c++){
+        soma += valores[c]
     }
 
-    vet.push(n)
-    c++
+    res.innerHTML = `<p>Ao todo, temos ${valores.length} números cadastrados.</p>`
+    res.innerHTML += `<p>O maior valor informado foi ${maxValue}.</p>`
+    res.innerHTML += `<p>O menor valor informado foi ${minValue}.</p>`
+    res.innerHTML += `<p>Somando todos os valores, temos ${soma}.</p>`
+    res.innerHTML += `<p>A média dos valores digitados é ${soma/valores.length}.</p>`
 }
 
 
